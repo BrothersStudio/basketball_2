4ba2d1d33d822da71a9c5de036a9b77a91427281
Patrick <pwcarney@bu.edu>
2018-07-08 16:26:06 -0400

    Made rudimentary field

---
+++ Assets/Scripts/PhaseController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseController : MonoBehaviour
+{
+    Phase current_phase = Phase.BlueAct;
+
+    void ChangePhase()
+    {
+        if (current_phase == Phase.BlueAct)
+        {
+            current_phase = Phase.RedAct;
+        }
+        else if (current_phase == Phase.RedAct)
+        {
+            current_phase = Phase.BlueAct;
+        }
+    }
+}
+
+public enum Phase
+{
+    BlueAct,
+    RedAct
+}
