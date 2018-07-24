43ab81f14c93a3abafd74982cbaee326a049a3ae
Patrick <pwcarney@bu.edu>
2018-07-23 22:20:08 -0400

    Added sprite scores

--- Assets/Scripts/UI/ScoreCounter.cs
+++ Assets/Scripts/UI/ScoreCounter.cs
@@ -8,6 +8,8 @@ public class ScoreCounter : MonoBehaviour
     int team_A_score = 0;
     int team_B_score = 0;
 
+    public List<Sprite> number_sprites;
+
     public void PlayerScored(Player player)
     {
         if (player.team == Team.A)
@@ -23,12 +25,23 @@ public class ScoreCounter : MonoBehaviour
     void TeamAScores(int points)
     {
         team_A_score += points;
-        transform.Find("Team A BG/Score").GetComponent<Text>().text = team_A_score.ToString();
+        ConvertToSpriteNumbers(team_A_score, transform.Find("Home Score").gameObject);
     }
 
     void TeamBScores(int points)
     {
         team_B_score += points;
-        transform.Find("Team B BG/Score").GetComponent<Text>().text = team_B_score.ToString();
+        ConvertToSpriteNumbers(team_B_score, transform.Find("Away Score").gameObject);
+    }
+
+    void ConvertToSpriteNumbers(int score, GameObject panel)
+    {
+        string string_score = score.ToString("00");
+
+        char tens = string_score[0];
+        char ones = string_score[1];
+
+        panel.transform.Find("Tens").GetComponent<Image>().sprite = number_sprites[int.Parse(tens.ToString())];
+        panel.transform.Find("Ones").GetComponent<Image>().sprite = number_sprites[int.Parse(ones.ToString())];
     }
 }
