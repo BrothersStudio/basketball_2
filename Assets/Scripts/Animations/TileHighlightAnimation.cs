fd0a57f1e2fe72ca4cf20b65072d408fbd0bbbf6
Patrick <pwcarney@bu.edu>
2018-07-25 21:29:44 -0400

    Added tile highlight animation

---
+++ Assets/Scripts/Animations/TileHighlightAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileHighlightAnimation : MonoBehaviour
+{
+    public Sprite tile_1;
+    public Sprite tile_2;
+
+    int current_tile = 0;
+
+    int time_tile_change = 0;
+
+    void FixedUpdate()
+    {
+        time_tile_change++;
+        if (time_tile_change > 15)
+        {
+            time_tile_change = 0;
+            SwitchSprite();
+        }
+    }
+
+    void SwitchSprite()
+    {
+        if (current_tile == 0)
+        {
+            current_tile = 1;
+            GetComponent<SpriteRenderer>().sprite = tile_2;
+        }
+        else
+        {
+            current_tile = 0;
+            GetComponent<SpriteRenderer>().sprite = tile_1;
+        }
+    }
+}
