3a764e78cca3109b94f8a9ba276305e5e4e90e06
Patrick <pwcarney@bu.edu>
2018-07-29 13:00:34 -0400

    Tutorial is more concise and fixed the text box position

--- Assets/Scripts/GameState/TutorialController.cs
+++ Assets/Scripts/GameState/TutorialController.cs
@@ -12,7 +12,7 @@ public class TutorialController : MonoBehaviour
     bool performing_action = false;
     Highlight highlight;
 
-    float last_text_time = 0;
+    float last_text_time = Mathf.Infinity;
 
     int tutorial_ind = -1;
     List<string> tutorial_strings = new List<string>();
@@ -31,41 +31,30 @@ public class TutorialController : MonoBehaviour
     void SetTutorialStrings()
     {
         // Start
-        tutorial_strings.Add("Welcome to Basketball 2!\nAre you ready to jam?");
-        tutorial_strings.Add("The goal is to get the ball\nto the hoop.");
-        tutorial_strings.Add("Once you do so,\nyou score!");
-        tutorial_strings.Add("However, if the defense\nholds you off");
-        tutorial_strings.Add("Until the shot clock\ngoes off...");
-        tutorial_strings.Add("They get the ball!");
-        tutorial_strings.Add("After 4 quarters,\na winner is called!");
-        tutorial_strings.Add("I'll show you\nthe first action!");
+        tutorial_strings.Add("Welcome to Basketball 2! Are you ready to jam?");
+        tutorial_strings.Add("Your goal is to get the ball to the hoop.");
+        tutorial_strings.Add("However, if the defense holds you off until the shot clock goes off, they get the ball!");
+        tutorial_strings.Add("I'll show you the first action type!");
 
         // Action
         tutorial_strings.Add("Doing move and push...");
 
-        tutorial_strings.Add("When you aren't\nholding the ball");
-        tutorial_strings.Add("You can move\n2 spaces");
-        tutorial_strings.Add("And you can push people!");
-        tutorial_strings.Add("Be careful! You can\nbe pushed off the edge");
-        tutorial_strings.Add("Or into stage hazards!");
-        tutorial_strings.Add("If you're holding the ball\nwhen that happens...");
-        tutorial_strings.Add("The opponent gets possession!");
+        tutorial_strings.Add("When you aren't holding the ball, you can move 2 spaces!");
+        tutorial_strings.Add("And you can push people out of the way or even off the stage!");
+        tutorial_strings.Add("If you're holding the ball when you fall, the opponent gets it!");
 
         // Action
         tutorial_strings.Add("Ally falling");
 
-        tutorial_strings.Add("Fortunately, you won't\nbe alone!");
-        tutorial_strings.Add("You can pass the ball\nto allies within 3 squares");
-        tutorial_strings.Add("Holding the ball\nallows you to move 3 squars");
-        tutorial_strings.Add("Instead of 2 without");
-        tutorial_strings.Add("But remember,\nyou can't push while holding the ball!");
-        tutorial_strings.Add("Pass and score!");
+        tutorial_strings.Add("Fortunately, you won't be alone!");
+        tutorial_strings.Add("You can pass the ball to allies within 3 squares of you.");
+        tutorial_strings.Add("Holding the ball\nallows you to move 3 squares, instead of the 2 without.");
+        tutorial_strings.Add("Select yourself, pass to your teammate, and score!");
 
         // Action
         tutorial_strings.Add("Player control");
 
-        tutorial_strings.Add("Nice! You're ready\nfor the big leagues!");
-        tutorial_strings.Add("Good luck!");
+        tutorial_strings.Add("Nice! You're ready for the big league! Good luck!");
     }
 
     public void StartTutorial()
@@ -93,7 +82,13 @@ public class TutorialController : MonoBehaviour
         tutorial.can_pass = true;
 
         tutorial_talker = FindObjectsOfType<Player>()[0];
-        tutorial_text.transform.parent.GetComponent<FollowFocusPlayer>().SetFocusPlayer(tutorial_talker.gameObject);
+        StartCoroutine(DelayAppear());
+    }
+
+    IEnumerator DelayAppear()
+    {
+        yield return new WaitForSeconds(2);
+        last_text_time = Time.timeSinceLevelLoad;
         tutorial_text.transform.parent.gameObject.SetActive(true);
     }
 
@@ -118,17 +113,17 @@ public class TutorialController : MonoBehaviour
     {
         switch (ind)
         {
-            case 8:
+            case 4:
                 performing_action = true;
                 tutorial_text.transform.parent.gameObject.SetActive(false);
                 StartCoroutine(MoveAndPush());
                 return true;
-            case 16:
+            case 8:
                 performing_action = true;
                 tutorial_text.transform.parent.gameObject.SetActive(false);
                 StartCoroutine(SpawnAlly());
                 return true;
-            case 23:
+            case 13:
                 performing_action = true;
                 tutorial_text.transform.parent.gameObject.SetActive(false);
                 PlayerControl();
