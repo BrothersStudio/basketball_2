22aa98c4efc23a1943c6afe4d7c5cddddf68cb36
Patrick <pwcarney@bu.edu>
2018-07-30 23:52:24 -0400

    Added blurred crowd to game and main menu

---
+++ Assets/Scripts/Animations/CrowdMotion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdMotion : MonoBehaviour
+{
+    int flip_speed = 0;
+
+    int current_ind = 0;
+    public Sprite[] crowd_sprite;
+
+	void FixedUpdate ()
+    {
+        flip_speed++;
+        if (flip_speed > 10)
+        {
+            flip_speed = 0;
+
+            current_ind++;
+            if (current_ind == crowd_sprite.Length)
+            {
+                current_ind = 0;
+            }
+
+            GetComponent<SpriteRenderer>().sprite = crowd_sprite[current_ind];
+        }
+	}
+}
