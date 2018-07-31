f022313a482ee17d0a9c85fad92954582bdec33f
Patrick <pwcarney@bu.edu>
2018-07-30 20:33:46 -0400

    Added second battle theme

---
+++ Assets/Scripts/Camera/SongSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongSelector : MonoBehaviour
+{
+    public AudioClip battle_song_1;
+    public AudioClip battle_song_2;
+
+	void Awake ()
+    {
+		switch (Progression.level)
+        {
+            case 0:
+                GetComponent<AudioSource>().clip = battle_song_2;
+                break;
+            case 1:
+                GetComponent<AudioSource>().clip = battle_song_1;
+                break;
+            case 2:
+                GetComponent<AudioSource>().clip = battle_song_2;
+                break;
+            case 3:
+                GetComponent<AudioSource>().clip = battle_song_1;
+                break;
+        }
+        GetComponent<AudioSource>().Play();
+	}
+}
